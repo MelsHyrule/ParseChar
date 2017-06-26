1052115fb2ce69024452af9f1e0a53af53f6f7e3
Melody Ann Seda Marotte <melshyrule@fb.com>
2017-06-26 15:45:52 -0700

    Milestone 4: View a List of Chat Messages

--- ParseChat/ChatViewController.swift
+++ ParseChat/ChatViewController.swift
@@ -9,16 +9,23 @@
 import UIKit
 import Parse
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var chatMessageTextField: UITextField!
+    @IBOutlet weak var chatTableView: UITableView!
+    
+    var messages: [PFObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        chatTableView.dataSource = self
+        
         // Do any additional setup after loading the view.
-    }
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.onTimer), userInfo: nil, repeats: true)
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,7 +42,6 @@ class ChatViewController: UIViewController {
             }
         }
         chatMessageTextField.text = ""
-        
     }
 
     /*
@@ -47,5 +53,35 @@ class ChatViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatCell
+        let message = messages[indexPath.row]
+        print(message)
+        cell.messageLabel.text = message["text"] as! String
+        return cell
+    }
+    
+    func onTimer() {
+        // Add code to be run periodically
+        let query = PFQuery(className: "Message_fbu2017")
+        query.addDescendingOrder("createdAt")
+        query.findObjectsInBackground { (messages: [PFObject]?, error: Error?) in
+            if let messages = messages {
+                // do something with the array of object returned by the call
+                self.messages = messages
+                self.chatTableView.reloadData()
+            } else {
+                print(error?.localizedDescription as? String)
+            }
+        }
+        // The getObjectInBackgroundWithId methods are asynchronous, so any code after this will run
+        // immediately.  Any code that depends on the query result should be moved
+        // inside the completion block above.
+    }
 
 }
