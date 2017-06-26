ef0cab2ae50e1516b3a8a0086861f36588d80f0b
Melody Ann Seda Marotte <melshyrule@fb.com>
2017-06-26 11:34:30 -0700

    Set up Parse

---
+++ ParseChat/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  ParseChat
+//
+//  Created by Melody Ann Seda Marotte on 6/26/17.
+//  Copyright © 2017 Melody Ann Seda Marotte. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
